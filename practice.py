d67b043badafa38110e05500a78f17e457f9f26e
sucanpa <s230207s@st.tuat.ac.jp>
2025-05-31 14:50:03 +0900

    コミット練習

---
+++ practice.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import datetime 
+
+st.title('Hello, Streamlit!')
+ 
+st.write('変数を設-定')
+count = 0
+ 
+if 'count' not in st.session_state:
+    st.session_state.count = 0
+ 
+if st.button('push me'):
+    st.write('ボタンを押しました')
+    st.session_state.count += 1
+ 
+st.write('count = ', st.session_state.count)
+
+d = st.date_input('誕生日を入力してください。', datetime.date(1998, 2, 16))
+st.write('私の誕生日は：', d)
